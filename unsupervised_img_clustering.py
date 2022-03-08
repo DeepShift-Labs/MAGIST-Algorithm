ab1695645d06322b5990a162de8a393c13b414ad
Krishna Shah <kshahusa@gmail.com>
2022-03-08 14:28:30 -0600

    Testing entire system together
    some bugs like google's false detetion.

--- unsupervised_img_clustering.py
+++ unsupervised_img_clustering.py
@@ -1,19 +1,11 @@
-from PIL import Image
-import numpy as np
-from tqdm import tqdm
 import pandas as pd
 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import colors
-from skimage.color import rgb2gray, rgb2hsv, hsv2rgb
-from skimage.io import imread, imshow
+from skimage.io import imread, imshow, imsave
+from skimage.transform import resize
 from sklearn.cluster import KMeans
-
-
-img = imread('Initial_Img/a/frame1.jpg')
-plt.figure(num=None, figsize=(8, 6), dpi=80)
-imshow(img)
+from skimage.util import img_as_uint
+import search
 
 def image_to_pandas(image):
     df = pd.DataFrame([image[:,:,0].flatten(),
@@ -21,25 +13,39 @@ def image_to_pandas(image):
                        image[:,:,2].flatten()]).T
     df.columns = ['Red_Channel','Green_Channel','Blue_Channel']
     return df
-df_img = image_to_pandas(img)
 
-print(df_img.head())
 
-kmeans = KMeans(n_clusters=3, random_state=0).fit(df_img)
-print(kmeans.labels_)
+def unsupervised_clusters(n_of_clusters, img_location, img_size, masked_img_dir):
+    img = imread(img_location)
+    img = resize(img, img_size)
+    plt.figure(num=None, figsize=(8, 6), dpi=80)
+    imshow(img)
 
-result = kmeans.labels_.reshape(img.shape[0],img.shape[1])
-imshow(result, cmap='viridis')
-plt.show()
 
-fig, axes = plt.subplots(1,3, figsize=(15, 12))
-for n, ax in enumerate(axes.flatten()):
-    img2 = imread('Initial_Img/a/frame1.jpg')
-    img2[:, :, 0] = img2[:, :, 0]*(result==[n])
-    img2[:, :, 1] = img2[:, :, 1]*(result==[n])
-    img2[:, :, 2] = img2[:, :, 2]*(result==[n])
-    ax.imshow(img2);
-    ax.set_axis_off()
-fig.tight_layout()
+    df_img = image_to_pandas(img)
 
-plt.show()
+    print(df_img.head())
+
+    kmeans = KMeans(n_clusters=n_of_clusters, random_state=0).fit(df_img)
+    print(kmeans.labels_)
+
+    result = kmeans.labels_.reshape(img.shape[0],img.shape[1])
+    imshow(result, cmap='viridis')
+    plt.show()
+
+    fig, axes = plt.subplots(1,n_of_clusters, figsize=(15, 12))
+    for n, ax in enumerate(axes.flatten()):
+        img2 = imread(img_location)
+        img2 = resize(img2, img_size)
+        img2[:, :, 0] = img2[:, :, 0]*(result==[n])
+        img2[:, :, 1] = img2[:, :, 1]*(result==[n])
+        img2[:, :, 2] = img2[:, :, 2]*(result==[n])
+        unit_img = img_as_uint(img2)
+        imsave(f'{masked_img_dir}/masked{n}.jpg', unit_img)
+        ax.imshow(img2);
+        ax.set_axis_off()
+    fig.tight_layout()
+    plt.show()
+
+unsupervised_clusters(3, 'test.jpg', (540, 480), ".")
+search.reverse_image_search('masked1.jpg')
