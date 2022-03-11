12914f766638f6ceab8211a0bdb267edca823011
Krishna Shah <kshahusa@gmail.com>
2022-03-10 23:32:42 -0600

    Updated the data downloader with no luck

---
+++ data_downlink.py
@@ -0,0 +1,22 @@
+import fiftyone as fo
+
+dataset = fo.zoo.load_zoo_dataset(
+              "open-images-v6",
+              split="validation",
+              label_types=["detections"],
+              classes=["Cat", "Dog"],
+              max_samples=100,
+          )
+
+patches = dataset.to_patches("detections")
+
+# The `ground_truth` field has type `Detection`, but COCO format expects
+# `Detections`, so the labels are automatically coerced to single-label lists
+patches.export(
+    export_dir="Data2",
+    dataset_type=fo.types.TFObjectDetectionDataset,
+    label_field="detections",
+)
+
+
+
