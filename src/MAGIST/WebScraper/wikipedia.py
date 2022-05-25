3759545cdc13eea60398ba85c985ddf0eaceba78
Krishna Shah <kshahusa@gmail.com>
2022-05-24 21:17:59 -0500

    Finalized wikipedia and started datadownloader

--- src/MAGIST/WebScraper/wikipedia.py
+++ src/MAGIST/WebScraper/wikipedia.py
@@ -9,22 +9,26 @@ class WikipediaScraper:
 
 	def get_summary(self, keyword):
 		result = wikipedia.search(keyword)
+		self.log.info("Wikipedia keyword search successful")
 		for r in result:
 			try:
 				page = wikipedia.page(r, auto_suggest=False, redirect=False)
 				summary = page.summary
 				break
 			except:
+				self.log.warning("Wikipedia search retreival failed! Retrying with different search result...")
 				pass
 		return summary
 
 	def get_title(self, keyword):
 		result = wikipedia.search(keyword)
+		self.log.info("Wikipedia keyword search successful")
 		for r in result:
 			try:
 				page = wikipedia.page(r, auto_suggest=False, redirect=False)
 				title = page.title
 				break
 			except:
+				self.log.warning("Wikipedia search retreival failed! Retrying with different search result...")
 				pass
 		return title
